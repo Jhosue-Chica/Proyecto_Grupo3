cc4795b7d638ac629657c735b632c19b363b5ee5
Mateo Beltrán <105222194+MateoBel20@users.noreply.github.com>
2024-12-12 12:44:46 -0500

    Actualización Página Principal
    Se arreglaron los estilos de la página

--- src/app/mateo-pantalla-principal/mateo-pantalla-principal.component.ts
+++ src/app/mateo-pantalla-principal/mateo-pantalla-principal.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-mateo-pantalla-principal',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterModule],
   templateUrl: './mateo-pantalla-principal.component.html',
   styleUrl: './mateo-pantalla-principal.component.css'
 })
 export class MateoPantallaPrincipalComponent {
+  crearPartida() {
+    console.log('Crear Partida clickeado');
+    // Implementa la lógica para crear una partida
+  }
 
+  unirsePartida() {
+    console.log('Unirse a Partida clickeado');
+    // Implementa la lógica para unirse a una partida
+  }
 }
