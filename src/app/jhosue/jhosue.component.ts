efb68df7bbefe8686a4fb15b853c7946920254e8
Mateo Beltrán <105222194+MateoBel20@users.noreply.github.com>
2024-12-12 14:03:10 -0500

    Ruteo del componente Lobby
    Se implementó la opción de cancelar en el lobby

--- src/app/jhosue/jhosue.component.ts
+++ src/app/jhosue/jhosue.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-jhosue',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './jhosue.component.html',
   styleUrl: './jhosue.component.css'
 })
