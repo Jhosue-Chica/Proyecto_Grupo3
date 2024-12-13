e09575f8b9bb90cb37bf1b674d732339eb26c40e
Brithney Salazar <93927947+Brithn@users.noreply.github.com>
2024-12-13 08:46:28 -0500

    update-table

--- src/app/brithney/brithney.component.ts
+++ src/app/brithney/brithney.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-brithney',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,RouterModule],
   templateUrl: './brithney.component.html',
   styleUrl: './brithney.component.css'
 })
 export class BrithneyComponent {
+
   columnas = ['Juan', 'María', 'Pedro', 'Ana']; // Nombres de personas
   filas = [
     { nombre: '1/3', valores: ['', '', '', ''] },
@@ -21,23 +23,26 @@ export class BrithneyComponent {
     { nombre: 'Escalera', valores: ['', '', '', ''] },
   ];
 
+
+  // Calcula la suma de cada columna
+  getSumaColumna(columnaIndex: number): number {
+    return this.filas.reduce((sum, fila) => {
+      const valor = parseFloat(fila.valores[columnaIndex]);
+      return sum + (isNaN(valor) ? 0 : valor);
+    }, 0);
+  }
+
+ // Evita el cambio de campo al presionar Enter
+ preventEnter(event: KeyboardEvent): void {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+  }
+}
   // Función para asignar colores a cada columna
   getColumnClass(index: number): string {
     const colors = ['custom-bg-1', 'custom-bg-2', 'custom-bg-3', 'custom-bg-4'];
     return colors[index % colors.length]; // Asigna un color cíclico
   }
 
-  // Calcula la suma de cada columna
-  getSumaColumna(columnaIndex: number): number {
-    return this.filas.reduce((suma, fila) => {
-      const valor = parseFloat(fila.valores[columnaIndex]) || 0;
-      return suma + valor;
-    }, 0);
-  }
 
-  // Función para manejar el evento input
-  onInputChange(event: any, columnIndex: number) {
-    // Puedes manejar el valor ingresado aquí si es necesario
-    console.log('Valor ingresado:', event.target.value);
-  }
 }
